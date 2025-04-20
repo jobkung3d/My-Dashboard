36fb90aef321e6e922251261fce74de3b9a8fa7e
jobkung3d <worachat0137@gmail.com>
2025-04-20 11:43:51 +0700

    update insert post date

--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -1,14 +1,56 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Link from 'next/link'
 import Container from '../components/Container'
 import { useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 function CreatePage() {
     const { data: session } = useSession();
+    if (!session) redirect("/login")
+
+    const userEmail = session?.user?.email;
+
+    const [title, setTitle] = useState("");
+    const [img, setImg] = useState("");
+    const [content, setContent] = useState("");
+
+    const router = useRouter();
+
+    console.log(title, img, content)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!title || !img || !content) {
+            alert("Please Complete All Inputs. ");
+            return;
+        }
+
+        try {
+            const res = await fetch("http://localhost:3000/api/posts", {
+                method: "POST",
+                header: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    title, img, content, userEmail
+                })
+            });
+
+            if (res.ok) {
+                router.push("/welcome")
+            } else {
+                throw new Error("Failed to create post")
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <Container>
@@ -18,10 +60,10 @@ function CreatePage() {
                     <Link href="/welcome" className="bg-gray-500 inline-block text-white border py-2 px-3 rounded my-2">Go back</Link>
                     <hr className="my-3" />
                     <h3 className="text-xl">Create Post</h3>
-                    <form action="">
-                        <input type="text" className="w-[300px] block bg-gray-200 border py-2 px-3 rounded text-lg my-2" placeholder='Post title' />
-                        <input type="text" className="w-[300px] block bg-gray-200 border py-2 px-3 rounded text-lg my-2" placeholder='Img url' />
-                        <textarea name="" id="" cols="30" rows="10" className="w-[300px] block bg-gray-200 border py-2 px-3 rounded text-lg my-2" placeholder='Enter your post content'>
+                    <form action="" onSubmit={handleSubmit}>
+                        <input type="text" onChange={(e) => setTitle(e.target.value)} className="w-[300px] block bg-gray-200 border py-2 px-3 rounded text-lg my-2" placeholder='Post title' />
+                        <input type="text" onChange={(e) => setImg(e.target.value)} className="w-[300px] block bg-gray-200 border py-2 px-3 rounded text-lg my-2" placeholder='Img url' />
+                        <textarea onChange={(e) => setContent(e.target.value)} cols="30" rows="10" className="w-[300px] block bg-gray-200 border py-2 px-3 rounded text-lg my-2" placeholder='Enter your post content'>
 
                         </textarea>
                         <button type='submit' name='create' className="bg-green-500 text-white border py-2 px-3 rounded text-lg my-2">Create Post</button>
